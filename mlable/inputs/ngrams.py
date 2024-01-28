60fb8d02a0c76b75bb6a9dfc2fd71823c477886d
apehex <apehex@protonmail.com>
2024-01-28 21:42:58 +0100

    Adapt the encoding to a continuous stream of text + take char 0 as blank (spacing because of the sorting)

--- mlable/inputs/ngrams.py
+++ mlable/inputs/ngrams.py
@@ -1,12 +1,23 @@
+import tensorflow as tf
+
+import mlable.inputs.vocabulary as _miv
+
 # CONSTANTS ###################################################################
 
 N_CONTEXT = 8
 BLANK = '$'
 
-# TEXT TO VECTOR ##############################################################
+# TEXT TO LIST ################################################################
 
 def tokenize(text: str, length: int=N_CONTEXT, blank=BLANK):
     __context = length * blank
     for __c in text:
         yield __context
         __context = __context[1:] + __c
+
+# TEXT TO VECTOR ##############################################################
+
+def dataset(text: list, stoi: callable, context: int=N_CONTEXT, depth: int=N_VOCABULARY) -> tuple:
+    __x = [_miv.encode(text=__n, stoi=stoi) for __n in tokenize(text=text, length=context)]
+    __y = _miv.encode(text=text, stoi=stoi)
+    return tf.convert_to_tensor(value=__x, dtype=tf.dtypes.int32), tf.one_hot(indices=__y, depth=depth, dtype=tf.dtypes.float32)
