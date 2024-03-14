b004e63c787d8da3e811e5dda907eba85f2b247f
apehex <apehex@protonmail.com>
2024-03-15 00:32:37 +0100

    Generalize the training function to be used with any optimizer + rebase the SGD on "torch.optim.Optimizer"

--- mlable/torch/optimizers.py
+++ mlable/torch/optimizers.py
@@ -1,49 +1,58 @@
 import torch
 
-import mlable.torch.datasets as _mtd
+import mlable.torch.data as _mtd
 
 # LEARNING RATE ###############################################################
 
-def rate(epoch: int, lr_mtn: float, lr_max: float, lr_exp: float, rampup: int, sustain: int) -> float:
-    __lr = lr_mtn
+def rate(epoch: int, lr_min: float, lr_max: float, lr_exp: float, rampup: int, sustain: int) -> float:
+    __lr = lr_min
     if epoch < rampup:
-        __lr = lr_mtn + (epoch * (lr_max - lr_mtn) / rampup)
+        __lr = lr_min + (epoch * (lr_max - lr_min) / rampup)
     elif epoch < rampup + sustain:
         __lr = lr_max
     else:
-        __lr = lr_mtn + (lr_max - lr_mtn) * lr_exp ** (epoch - rampup - sustain)
+        __lr = lr_min + (lr_max - lr_min) * lr_exp ** (epoch - rampup - sustain)
     return __lr
 
 # SGD #########################################################################
 
-def step(model: Sequential, loss: callable, x: torch.Tensor, y: torch.Tensor, lr: float=0.001) -> torch.Tensor:
+class SGD(torch.optim.Optimizer):
+    def __init__(self, params: list, rate: callable, **kwargs) -> None:
+        super(SGD, self).__init__(**kwargs)
+        self._parameters = list(params)
+        self._rate = rate
+
+    def step(epoch: int) -> None:
+        with torch.no_grad():
+            for __p in self._parameters:
+                __p -= self._rate(epoch) * __p.grad
+
+# GENERIC #####################################################################
+
+def step(model: torch.nn.Module, loss: callable, optimizer: torch.optim.Optimizer, x: torch.Tensor, y: torch.Tensor, epoch: int) -> torch.Tensor:
     # forward
-    __logits = model(x=x, training=True)
-    __loss = loss(input=__logits, target=y)
+    __output = model(x=x, training=True)
+    __loss = loss(input=__output, target=y)
     # backward
-    for __p in model.parameters(): __p.grad = None
+    model.zero_grad(set_to_none=True)
     __loss.backward()
-    # SGD update
-    with torch.no_grad():
-        for __p in model.parameters():
-            __p += -lr * __p.grad
+    # update the parameters
+    optimizer.step()
     return __loss
 
-def sgd(model:Sequential, x: torch.Tensor, y: torch.Tensor, n_epoch: int=N_EPOCHS, n_batch: int=N_BATCH) -> None:
+def train(model:torch.nn.Module, loss: callable, optimizer: torch.optim.Optimizer, x: torch.Tensor, y: torch.Tensor, n_epoch: int, n_batch: int) -> None:
     # scheme
     __steps = int(x.shape[0]) // n_batch
     # iterate on the whole dataset
     for __e in range(n_epoch):
-        # learning rate
-        __lr = rate(epoch=__e, lr_mtn=R_MIN, lr_max=R_MAX, lr_exp=R_EXP, rampup=4, sustain=0)
         # iterate on batchs
         for __s in range(__steps):
             # track the overall iteration
             __k = __e * __steps + __s
             # random batch
-            __x, __y = batch(x=x, y=y, size=n_batch)
+            __x, __y = _mtd.batch(x=x, y=y, size=n_batch)
             # step
-            __loss = step(model=model, loss=torch.nn.functional.cross_entropy, x=__x, y=__y, lr=__lr)
+            __loss = step(model=model, loss=loss, optimizer=optimizer, x=__x, y=__y, epoch=__e)
             # log the progress
             if __s % __steps == 0:
                 print('[epoch {epoch}] train loss: {train}'.format(epoch=__e, train=__loss.item()))
