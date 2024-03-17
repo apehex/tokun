cb019e1c011a4787f1113fbaa0ed9b0b688cc6f3
apehex <apehex@protonmail.com>
2024-03-17 17:00:44 +0100

    Simplify the sample generation + random choice of next token

--- mlable/tensorflow/sampling.py
+++ mlable/tensorflow/sampling.py
@@ -1,19 +1,16 @@
-import random
-
 import tensorflow as tf
 
 # SAMPLE ######################################################################
 
 def _next(model: tf.Module, x: tf.Tensor) -> int:
-    __prob = tf.squeeze(model(x, training=False))
-    return tf.argmax(__prob, axis=-1).numpy()
+    __prob = model(x, training=False)
+    return tf.squeeze(tf.random.categorical(tf.math.log(__prob), 1)).numpy()
 
 def sample(model: tf.Module, context: int, depth: int, length: int) -> str:
-    __result = [int(random.uniform(0, depth))]
-    __ngram = (context - 1) * [0,] + __result
+    __result = []
+    __ngram = context * [0]
     for __i in range(length):
-        __x = tf.convert_to_tensor(value=[__ngram], dtype=tf.dtypes.int32)
-        __n = _next(model=model, x=__x)
+        __n = _next(model=model, x=tf.convert_to_tensor(value=[__ngram], dtype=tf.dtypes.int32))
         __ngram = __ngram[1:] + [__n]
         __result.append(__n)
     return __result
