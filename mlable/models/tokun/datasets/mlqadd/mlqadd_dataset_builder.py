61c91a4de44874fb21280b961e5b8b0f4377035f
apehex <apehex@protonmail.com>
2024-04-11 22:32:22 +0200

    Generate data (uni)code after code

--- mlable/models/tokun/datasets/mlqadd/mlqadd_dataset_builder.py
+++ mlable/models/tokun/datasets/mlqadd/mlqadd_dataset_builder.py
@@ -5,6 +5,8 @@ import itertools
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+import mlable.models.tokun.datasets.pipeline as _mmad
+
 # GENERIC #####################################################################
 
 def _cycle(iterators: list) -> iter:
@@ -38,12 +40,12 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     VERSION = tfds.core.Version('1.0.0')
     RELEASE_NOTES = {'1.0.0': 'Initial release.',}
 
-    def __init__(self, train_lang: list=['en'], test_lang: list=['en'], batch_size: int=128, **kwargs) -> None:
+    def __init__(self, train_lang: list=['en'], test_lang: list=['es'], **kwargs) -> None:
         super(Builder, self).__init__(**kwargs)
         # track sample ids, used when saving to disk
         self._train_id = -1
         self._test_id = -1
-        # batch iterators, one per language
+        # batch iterators, one sample per query, one iterator per language
         self._train_iter = _cycle(iterators=[_load(lang=__l, split='test', **kwargs) for __l in train_lang])
         self._test_iter = _cycle(iterators=[_load(lang=__l, split='validation', **kwargs) for __l in test_lang])
 
@@ -51,10 +53,11 @@ class Builder(tfds.core.GeneratorBasedBuilder):
         """Returns the dataset metadata."""
         return self.dataset_info_from_configs(
             homepage='https://github.com/apehex/mlable/',
-            supervised_keys=None,
+            supervised_keys=('utf-32', 'utf-32'),
             disable_shuffling=False,
             features=tfds.features.FeaturesDict({
-                'text': tfds.features.Text(),})) # TODO (None, 512 after tokenization)
+                # 'bytes': tfds.features.Text(),
+                'utf-32': tfds.features.Tensor(shape=(4, 256), dtype=tf.dtypes.float32)}))
 
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Generates the data splits."""
@@ -63,10 +66,17 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     def _generate_examples(self, train: bool=True) -> iter:
         """Produces long text samples with mixed languages."""
         if train:
-            for __b in self._train_iter:
-                self._train_id += 1
-                yield self._train_id, {'text': tf.reshape(tensor=_merge(batch=__b), shape=()).numpy()}
+            for __batch in self._train_iter: # single sample in batch
+                __utf8 = tf.reshape(tensor=_merge(batch=__batch), shape=()).numpy()
+                __utf32 = _mmad.tokenize(text=__utf8.decode('utf-8'))
+                for __i in range(__utf32.shape[0]):
+                    self._train_id += 1
+                    yield self._train_id, {'utf-32': tf.one_hot(indices=__utf32[__i], depth=256, dtype=tf.dtypes.float32).numpy()}
         else:
-            for __b in self._test_iter:
+            for __b in self._test_iter: # single sample in batch
                 self._test_id += 1
-                yield self._test_id, {'text': tf.reshape(tensor=_merge(batch=__b), shape=()).numpy()}
+                __utf8 = tf.reshape(tensor=_merge(batch=__b), shape=()).numpy()
+                __utf32 = _mmad.tokenize(text=__utf8.decode('utf-8'))
+                for __i in range(__utf32.shape[0]):
+                    self._test_id += 1
+                    yield self._test_id, {'utf-32': tf.one_hot(indices=__utf32[__i], depth=256, dtype=tf.dtypes.float32).numpy()}
