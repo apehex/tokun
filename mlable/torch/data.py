e14f7fe4f32336777cacef9e039b5f95e8292da8
apehex <apehex@protonmail.com>
2024-05-04 15:09:14 +0200

    Split from "mlable" + tidy
    Former-commit-id: 4bf0169ff6713678b3057e5e37001ddebd6b6d0a

--- mlable/torch/data.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-# BATCH #######################################################################
-
-def batch(x: torch.Tensor, y: torch.Tensor, size: int) -> tuple:
-    __indices = torch.randint(0, x.shape[0], (size,))
-    return x[__indices], y[__indices]
