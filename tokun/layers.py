ae6bba56bc7c1910787602eb06777011ec73b6ba
apehex <apehex@protonmail.com>
2024-06-05 08:41:22 +0200

    Generalize the layers (no hypothesis on axes) + stop exposing internal parameters
    Former-commit-id: e15b70c8ea2ebc81a66c4708f88245fc760c0dbe

--- tokun/layers.py
+++ tokun/layers.py
@@ -12,8 +12,7 @@ import mlable.layers.reshaping
 class TokenizeBlock(tf.keras.layers.Layer):
     def __init__(
         self,
-        left_axis: int=-2,
-        right_axis: int=-1,
+        feature_axis: int=-1,
         token_dim: int=4,
         latent_dim: int=256,
         attention: bool=False,
@@ -22,31 +21,34 @@ class TokenizeBlock(tf.keras.layers.Layer):
         **kwargs
     ) -> None:
         super(TokenizeBlock, self).__init__(**kwargs)
+        # this axis is inserted and then merged
+        __temp_axis = 1
+        # config
+        self._config = {
+            'feature_axis': feature_axis,
+            'token_dim': token_dim,
+            'latent_dim': latent_dim,
+            'attention': attention,
+            'normalization': normalization,
+            'activation': activation,}
         # layers
-        self._normalization = tf.keras.layers.LayerNormalization(axis=-1, epsilon=0.001, center=True, scale=True, name='normalization') if normalization else None # normalize each token unit independently
-        self._divide = mlable.layers.reshaping.Divide(input_axis=0, output_axis=1, factor=token_dim, insert=True, name='group') # (B * G, E) => (B, G, E)
-        self._embedding = mlable.layers.embedding.PositionalEmbedding(input_axis=left_axis, output_axis=right_axis, name='position') # (B, G, E) + (1, G, E)
-        self._attention = tf.keras.layers.Attention(use_scale=False, score_mode='dot', dropout=0., seed=None, name='attention') if attention else None # (B, G, E) + (B, G, E) * (B, E, G) * (B, G, E)
-        self._merge = mlable.layers.reshaping.Merge(left_axis=left_axis, right_axis=right_axis, left=True, name='merging') # (B, G, E) => (B, G * E)
+        self._normalization = tf.keras.layers.LayerNormalization(axis=feature_axis, epsilon=0.001, center=True, scale=True, name='normalization') if normalization else None # normalize each token unit independently
+        self._divide = mlable.layers.reshaping.Divide(input_axis=0, output_axis=__temp_axis, factor=token_dim, insert=True, name='group') # (B * G, E) => (B, G, E)
+        self._embedding = mlable.layers.embedding.PositionalEmbedding(input_axis=__temp_axis, output_axis=feature_axis, name='position') # (B, G, E) + (1, G, E)
+        self._attention = tf.keras.layers.MultiHeadAttention(num_heads=1, key_dim=latent_dim, value_dim=latent_dim, attention_axes=[__temp_axis], name='attention') if attention else None # (B, G, E) + (B, G, E) * (B, E, G) * (B, G, E)
+        self._merge = mlable.layers.reshaping.Merge(left_axis=__temp_axis, right_axis=feature_axis, left=False, name='merging') # (B, G, E) => (B, G * E)
         self._dense = tf.keras.layers.Dense(units=latent_dim, activation=activation, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', name='compression') # (B, G * E) => (B, L), typically L = E
 
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         __t = self._normalization(inputs) if self._normalization else inputs
         __t = self._embedding(self._divide(__t))
-        __t = self._attention([__t, __t, __t], return_attention_scores=False, use_causal_mask=False) if self._attention else __t
+        __t = self._attention(query=__t, key=__t, value=__t, return_attention_scores=False, use_causal_mask=False) if self._attention else __t
         return self._dense(self._merge(__t))
 
     def get_config(self) -> dict:
-        __parent_config = super(TokenizeBlock, self).get_config()
-        __child_config = {
-            'left_axis': self._merge._left_axis,
-            'right_axis': self._merge._right_axis,
-            'token_dim': self._divide._factor,
-            'latent_dim': self._dense.units,
-            'attention': bool(self._attention),
-            'normalization': bool(self._normalization),
-            'activation': self._dense.activation.__name__,}
-        return {**__parent_config, **__child_config}
+        __config = super(TokenizeBlock, self).get_config()
+        __config.update(self._config)
+        return __config
 
     @classmethod
     def from_config(cls, config) -> tf.keras.layers.Layer:
@@ -58,6 +60,7 @@ class TokenizeBlock(tf.keras.layers.Layer):
 class DetokenizeBlock(tf.keras.layers.Layer):
     def __init__(
         self,
+        feature_axis: int=-1,
         token_dim: int=4,
         embedding_dim: int=256,
         attention: bool=False,
@@ -66,29 +69,34 @@ class DetokenizeBlock(tf.keras.layers.Layer):
         **kwargs
     ) -> None:
         super(DetokenizeBlock, self).__init__(**kwargs)
+        # this axis is inserted and then merged
+        __temp_axis = 1
+        # config
+        self._config = {
+            'feature_axis': feature_axis,
+            'token_dim': token_dim,
+            'embedding_dim': embedding_dim,
+            'attention': attention,
+            'normalization': normalization,
+            'activation': activation,}
         # layers
         self._dense = tf.keras.layers.Dense(units=token_dim * embedding_dim, activation=activation, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', name='decompression') # (B, L) => (B, G * E), typically L = E
-        self._divide = mlable.layers.reshaping.Divide(input_axis=-2, output_axis=-1, insert=True, factor=embedding_dim, name='split') # (B, G * E) => (B, G, E)
-        self._embedding = mlable.layers.embedding.PositionalEmbedding(input_axis=-2, output_axis=-1, name='position') # (B, G, E) + (1, G, E)
-        self._attention = tf.keras.layers.Attention(use_scale=False, score_mode='dot', dropout=0., seed=None, name='attention') if attention else None # (B, G, E) + (B, G, E) * (B, E, G) * (B, G, E)
-        self._merge = mlable.layers.reshaping.Merge(left_axis=0, right_axis=1, left=True) # (B, G, E) => (B * G, E)
-        self._normalization = tf.keras.layers.LayerNormalization(axis=-1, epsilon=0.001, center=True, scale=True, name='normalization') if normalization else None # normalize each token unit independently
+        self._divide = mlable.layers.reshaping.Divide(input_axis=feature_axis, output_axis=__temp_axis, insert=True, factor=token_dim, name='split') # (B, G * E) => (B, G, E)
+        self._embedding = mlable.layers.embedding.PositionalEmbedding(input_axis=__temp_axis, output_axis=feature_axis, name='position') # (B, G, E) + (1, G, E)
+        self._attention = tf.keras.layers.MultiHeadAttention(num_heads=1, key_dim=embedding_dim, value_dim=embedding_dim, attention_axes=[__temp_axis], name='attention') if attention else None  # (B, G, E) + (B, G, E) * (B, E, G) * (B, G, E)
+        self._merge = mlable.layers.reshaping.Merge(left_axis=0, right_axis=__temp_axis, left=True) # (B, G, E) => (B * G, E)
+        self._normalization = tf.keras.layers.LayerNormalization(axis=feature_axis, epsilon=0.001, center=True, scale=True, name='normalization') if normalization else None # normalize each token unit independently
 
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         __t = self._embedding(self._divide(self._dense(inputs)))
-        __t = self._attention([__t, __t, __t], return_attention_scores=False, use_causal_mask=False) if self._attention else __t
+        __t = self._attention(query=__t, key=__t, value=__t, return_attention_scores=False, use_causal_mask=False) if self._attention else __t
         __t = self._merge(__t)
         return self._normalization(__t) if self._normalization else __t
 
     def get_config(self) -> dict:
-        __parent_config = super(DetokenizeBlock, self).get_config()
-        __child_config = {
-            'token_dim': self._dense.units // self._divide._factor,
-            'embedding_dim': self._divide._factor,
-            'attention': bool(self._attention),
-            'normalization': bool(self._normalization),
-            'activation': self._dense.activation.__name__,}
-        return {**__parent_config, **__child_config}
+        __config = super(DetokenizeBlock, self).get_config()
+        __config.update(self._config)
+        return __config
 
     @classmethod
     def from_config(cls, config) -> tf.keras.layers.Layer:
@@ -100,21 +108,24 @@ class DetokenizeBlock(tf.keras.layers.Layer):
 class HeadBlock(tf.keras.layers.Layer):
     def __init__(
         self,
+        feature_axis: int=-1,
         encoding_dim: int=256,
         **kwargs
     ) -> None:
         super(HeadBlock, self).__init__(**kwargs)
+        # config
+        self._config = {'feature_axis': feature_axis, 'encoding_dim': encoding_dim}
         # layers
-        self._dense = tf.keras.layers.Dense(units=encoding_dim, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', name='project-head') # (..., G, E) => (..., G, U), typically U = E
-        self._softmax = tf.keras.layers.Softmax(axis=-1, name='softmax') # (..., G, U)
+        self._dense = tf.keras.layers.Dense(units=encoding_dim, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', name='projection') # (..., G, E) => (..., G, U), typically U = E
+        self._softmax = tf.keras.layers.Softmax(axis=feature_axis, name='softmax') # (..., G, U)
 
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         return self._softmax(self._dense(inputs))
 
     def get_config(self) -> dict:
-        __parent_config = super(HeadBlock, self).get_config()
-        __child_config = {'encoding_dim': self._dense.units,}
-        return {**__parent_config, **__child_config}
+        __config = super(HeadBlock, self).get_config()
+        __config.update(self._config)
+        return __config
 
     @classmethod
     def from_config(cls, config) -> tf.keras.layers.Layer:
--- tokun/model.py
+++ tokun/model.py
@@ -10,31 +10,33 @@ import tokun.layers
 @keras.saving.register_keras_serializable(package='models')
 class Encoder(tf.keras.models.Model):
     def __init__(self, token_dim: list, encoding_dim: int, embedding_dim: int, latent_dim: int, batch_dim: int=None, attention: bool=False, normalization: bool=False, activation: str='silu', **kwargs) -> None:
+        # init
         super(Encoder, self).__init__(**kwargs)
+        # config
+        self._config = {
+            'token_dim': token_dim,
+            'encoding_dim': encoding_dim,
+            'embedding_dim': embedding_dim,
+            'latent_dim': latent_dim,
+            'batch_dim': batch_dim,
+            'attention': attention,
+            'normalization': normalization,
+            'activation': activation}
+        # successive dimensions of the merging units
+        __token_dim = [token_dim] if isinstance(token_dim, int) else token_dim
+        # layers
         self._encoder = tf.keras.Sequential([
             tf.keras.Input(shape=(encoding_dim,), batch_size=batch_dim, name='input'), # (B * G ^ D, U)
             tf.keras.layers.Dense(units=embedding_dim, activation=None, use_bias=False, kernel_initializer='glorot_uniform', bias_initializer=None, name='embed-1'),] # (B * G ^ D, U) => (B * G ^ D, E)
-            + [tokun.layers.TokenizeBlock(left_axis=-2, right_axis=-1, token_dim=__g, latent_dim=latent_dim, attention=attention, normalization=normalization, activation=activation, name='tokenize-{}_{}'.format(__g, __i)) for __i, __g in enumerate(token_dim)]) # (B * G ^ i, E) => (B * G ^ (i-1), E)
+            + [tokun.layers.TokenizeBlock(feature_axis=-1, token_dim=__g, latent_dim=latent_dim, attention=attention, normalization=normalization, activation=activation, name='tokenize-{}_{}'.format(__g, __i)) for __i, __g in enumerate(__token_dim)]) # (B * G ^ i, E) => (B * G ^ (i-1), E)
 
     def call(self, x: tf.Tensor) -> tf.Tensor:
         return self._encoder(x)
 
     def get_config(self) -> dict:
-        __parent_config = super(Encoder, self).get_config()
-        __input_shape = list(self._encoder.inputs[0].shape)
-        __embedding_config = self._encoder.layers[0].get_config()
-        __tokenizer_config = self._encoder.layers[1].get_config()
-        __token_dim = [__b.get_config().get('token_dim', 4) for __b in self._encoder.layers[1:]]
-        __child_config = {
-            'batch_dim': __input_shape[0],
-            'encoding_dim': __input_shape[-1],
-            'embedding_dim': __embedding_config['units'],
-            'token_dim': __token_dim,
-            'latent_dim': __tokenizer_config['latent_dim'],
-            'attention': __tokenizer_config['attention'],
-            'normalization': __tokenizer_config['normalization'],
-            'activation': __tokenizer_config['activation'],}
-        return {**__parent_config, **__child_config}
+        __config = super(Encoder, self).get_config()
+        __config.update(self._config)
+        return __config
 
     @classmethod
     def from_config(cls, config) -> tf.keras.layers.Layer:
@@ -45,31 +47,33 @@ class Encoder(tf.keras.models.Model):
 @keras.saving.register_keras_serializable(package='models')
 class Decoder(tf.keras.models.Model):
     def __init__(self, token_dim: list, encoding_dim: int, embedding_dim: int, latent_dim: int, batch_dim: int=None, attention: bool=False, normalization: bool=False, activation: str='silu', **kwargs) -> None:
+        # init
         super(Decoder, self).__init__(**kwargs)
+        # config
+        self._config = {
+            'token_dim': token_dim,
+            'encoding_dim': encoding_dim,
+            'embedding_dim': embedding_dim,
+            'latent_dim': latent_dim,
+            'batch_dim': batch_dim,
+            'attention': attention,
+            'normalization': normalization,
+            'activation': activation}
+        # successive dimensions of the dividing units
+        __token_dim = [token_dim] if isinstance(token_dim, int) else token_dim
+        # layers
         self._decoder = tf.keras.Sequential(
             [tf.keras.Input(shape=(latent_dim,), batch_size=batch_dim, name='input')] # (B, E)
-            + [tokun.layers.DetokenizeBlock(token_dim=__g, embedding_dim=embedding_dim, attention=attention, normalization=normalization, activation=activation, name='detokenize-{}_{}'.format(__g, __i)) for __i, __g in enumerate(token_dim)] # (B * G ^ i, E) => (B * G ^ (i+1), E)
-            + [tokun.layers.HeadBlock(encoding_dim=encoding_dim, name='project-head')]) # (B * G ^ D, E) => (B * G ^ D, U)
+            + [tokun.layers.DetokenizeBlock(feature_axis=-1, token_dim=__g, embedding_dim=embedding_dim, attention=attention, normalization=normalization, activation=activation, name='detokenize-{}_{}'.format(__g, __i)) for __i, __g in enumerate(__token_dim)] # (B * G ^ i, E) => (B * G ^ (i+1), E)
+            + [tokun.layers.HeadBlock(feature_axis=-1, encoding_dim=encoding_dim, name='project-head')]) # (B * G ^ D, E) => (B * G ^ D, U)
 
     def call(self, x: tf.Tensor) -> tf.Tensor:
         return self._decoder(x)
 
     def get_config(self) -> dict:
-        __parent_config = super(Decoder, self).get_config()
-        __input_shape = list(self._decoder.inputs[0].shape)
-        __detokenizer_config = self._decoder.layers[0].get_config()
-        __head_config = self._decoder.layers[-1].get_config()
-        __token_dim = [__b.get_config().get('token_dim', 4) for __b in self._encoder.layers[:-1]]
-        __child_config = {
-            'batch_dim': __input_shape[0],
-            'latent_dim': __input_shape[-1],
-            'encoding_dim': __head_config['encoding_dim'],
-            'token_dim': __detokenizer_config['token_dim'],
-            'embedding_dim': __detokenizer_config['embedding_dim'],
-            'attention': __detokenizer_config['attention'],
-            'normalization': __detokenizer_config['normalization'],
-            'activation': __detokenizer_config['activation'],}
-        return {**__parent_config, **__child_config}
+        __config = super(Decoder, self).get_config()
+        __config.update(self._config)
+        return __config
 
     @classmethod
     def from_config(cls, config) -> tf.keras.layers.Layer:
@@ -80,7 +84,9 @@ class Decoder(tf.keras.models.Model):
 @keras.saving.register_keras_serializable(package='models')
 class AutoEncoder(tf.keras.models.Model):
     def __init__(self, token_dim: list, encoding_dim: int, embedding_dim: int, latent_dim: int, batch_dim: int=None, attention: bool=False, normalization: bool=False, activation: str='silu', **kwargs) -> None:
+        # init
         super(AutoEncoder, self).__init__(**kwargs)
+        # layers
         self._encoder = Encoder(token_dim=token_dim, encoding_dim=encoding_dim, embedding_dim=embedding_dim, latent_dim=latent_dim, batch_dim=batch_dim, attention=attention, normalization=normalization, activation=activation)
         self._decoder = Decoder(token_dim=token_dim, encoding_dim=encoding_dim, embedding_dim=embedding_dim, latent_dim=latent_dim, batch_dim=batch_dim, attention=attention, normalization=normalization, activation=activation)
 
@@ -88,9 +94,9 @@ class AutoEncoder(tf.keras.models.Model):
         return self._decoder(self._encoder(x))
 
     def get_config(self) -> dict:
-        __parent_config = super(AutoEncoder, self).get_config()
-        __encoder_config = self._encoder.get_config()
-        return {**__encoder_config, **__parent_config}
+        __config = super(AutoEncoder, self).get_config()
+        __config.update(self._encoder.get_config())
+        return __config
 
     @classmethod
     def from_config(cls, config) -> tf.keras.layers.Layer:
