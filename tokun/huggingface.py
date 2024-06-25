5f71ac474919daa0ba8a759397b75f21d458e484
apehex <apehex@protonmail.com>
2024-06-25 19:03:47 +0200

    Accept any encoding for byte tokenization
    Former-commit-id: df1903419acd346dff03ac972a6ecf47aa5c4580

--- tokun/huggingface.py
+++ tokun/huggingface.py
@@ -6,24 +6,26 @@ import transformers
 # UTF-32-BE TOKENIZATION ######################################################
 
 class ByteTokenizer(transformers.PreTrainedTokenizer):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, encoding: str='utf-32-be', **kwargs) -> None:
         __kwargs = copy.deepcopy(kwargs)
         # special tokens
-        __kwargs['bos_token'] = kwargs.get('bos_token', '\u0002')
-        __kwargs['eos_token'] = kwargs.get('eos_token', '\u0003')
-        __kwargs['unk_token'] = kwargs.get('unk_token', '\u001a')
-        __kwargs['sep_token'] = kwargs.get('sep_token', '\u001d')
-        __kwargs['pad_token'] = kwargs.get('pad_token', '\u0000')
-        __kwargs['cls_token'] = kwargs.get('cls_token', '\u0011')
+        __kwargs['bos_token'] = __kwargs.get('bos_token', '\u0002')
+        __kwargs['eos_token'] = __kwargs.get('eos_token', '\u0003')
+        __kwargs['unk_token'] = __kwargs.get('unk_token', '\u001a')
+        __kwargs['sep_token'] = __kwargs.get('sep_token', '\u001d')
+        __kwargs['pad_token'] = __kwargs.get('pad_token', '\u0000')
+        __kwargs['cls_token'] = __kwargs.get('cls_token', '\u0011')
         # defaults
         __kwargs['split_special_tokens'] = True # 4 bytes for the special tokens too
         __kwargs['vocab_size'] = 256
+        # properties
         self._vocab_size = 256
+        self._encoding = encoding
         # init
         super(ByteTokenizer, self).__init__(**__kwargs)
 
     def _tokenize(self, text: str, **kwargs) -> list:
-        return list(chr(__b) for __b in text.encode('utf-32-be'))
+        return list(chr(__b) for __b in text.encode(self._encoding))
 
     def _convert_token_to_id(self, token: str) -> int:
         return ord(token)
@@ -32,7 +34,7 @@ class ByteTokenizer(transformers.PreTrainedTokenizer):
         return chr(index)
 
     def convert_tokens_to_string(self, tokens: iter) -> str:
-        return bytes(ord(__c) for __c in tokens).decode('utf-32-be')
+        return bytes(ord(__c) for __c in tokens).decode(self._encoding)
 
     def build_inputs_with_special_tokens(self, token_ids_0: list, token_ids_1: list=None) -> list:
         __cls_ids = [self._convert_token_to_id(__t) for __t in self.tokenize(self.cls_token, split_special_tokens=True)]
