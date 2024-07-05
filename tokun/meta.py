6380e9419d32ce1a14b76938461ff81b76649ae5
apehex <apehex@protonmail.com>
2024-07-05 13:00:27 +0200

    Adjust the optimizer settings
    Former-commit-id: df43e94cf148adaf15f2b05c3372da97e21715ec

--- tokun/meta.py
+++ tokun/meta.py
@@ -3,8 +3,8 @@
 def rates(pretrained: bool=False, normalization: bool=True, base: float=0.001) -> tuple:
     return (
         (0.1 if pretrained else 1.) * (0.1 if normalization else 1.) * base, # lr max
-        0.8, # beta_1 => decay for the first moment
-        0.9) # beta_2 => decay for the second moment
+        0.9, # beta_1 => decay for the first moment
+        0.99) # beta_2 => decay for the second moment
 
 def version(units: list, axis: int=1) -> list:
     return ['x'.join(str(__g) for __g in units), str(axis)]
