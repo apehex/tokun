c0ecdccc35b4e7f229127edcc6ca3a1336d83cb8
apehex <apehex@protonmail.com>
2024-07-14 17:33:21 +0200

    Generate binary samples
    Former-commit-id: b8e7053ef1af96c6573198c6fbe9288ca881390f

--- tokun/data.py
+++ tokun/data.py
@@ -9,18 +9,25 @@ import tensorflow as tf
 
 # UNICODE #####################################################################
 
-def random_codepoint(lower_plane: int=0, upper_plane: int=0x40000) -> list:
+def _random_codepoint_binary(lower_plane: int=0, upper_plane: int=0x40000) -> list:
+    __h = '{0:0>32b}'.format(int(random.uniform(lower_plane, upper_plane)))
+    return [int(__b) for __b in __h]
+
+def _random_codepoint_bytes(lower_plane: int=0, upper_plane: int=0x40000) -> list:
     __h = '{0:0>8x}'.format(int(random.uniform(lower_plane, upper_plane)))
     return list(bytes.fromhex(__h))
 
-def random_sample(sample_size: int, lower_plane: int=0, upper_plane: int=0x40000) -> list:
-    __nested = [random_codepoint(lower_plane=lower_plane, upper_plane=upper_plane) for _ in range(sample_size)]
+def random_codepoint(lower_plane: int=0, upper_plane: int=0x40000, binary: bool=False) -> list:
+    return _random_codepoint_binary(lower_plane=lower_plane, upper_plane=upper_plane) if binary else _random_codepoint_bytes(lower_plane=lower_plane, upper_plane=upper_plane)
+
+def random_sample(sample_size: int, lower_plane: int=0, upper_plane: int=0x40000, binary: bool=False) -> list:
+    __nested = [random_codepoint(lower_plane=lower_plane, upper_plane=upper_plane, binary=binary) for _ in range(sample_size)]
     return list(itertools.chain.from_iterable(__nested))
 
-def random_dataset(size: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x40000) -> tf.data.Dataset:
+def random_dataset(size: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x40000, binary: bool=False) -> tf.data.Dataset:
     def __generator() -> iter:
         for _ in range(size):
-            yield random_sample(sample_size=sample_size, lower_plane=lower_plane, upper_plane=upper_plane)
+            yield random_sample(sample_size=sample_size, lower_plane=lower_plane, upper_plane=upper_plane, binary=binary)
     return tf.data.Dataset.from_generator(
         generator=__generator,
         output_signature=tf.TensorSpec(shape=(4 * sample_size,), dtype=tf.int32))
