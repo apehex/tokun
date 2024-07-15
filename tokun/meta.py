2353d7a1305ba99ba776eb1ad3fb1157d9b7c54a
apehex <apehex@protonmail.com>
2024-07-15 20:37:38 +0200

    Distinguish trained models according to their encoding dimensions

--- tokun/meta.py
+++ tokun/meta.py
@@ -6,5 +6,5 @@ def rates(pretrained: bool=False, normalization: bool=True, base: float=0.001) -
         0.9, # beta_1 => decay for the first moment
         0.99) # beta_2 => decay for the second moment
 
-def version(units: list, axis: int=1) -> list:
-    return ['x'.join(str(__g) for __g in units), str(axis)]
+def version(token_units: list, sequence_axis: int=1, input_dim: int=256, output_dim: int=256) -> list:
+    return ['{}x{}'.format(input_dim, output_dim), 'x'.join(str(__u) for __u in token_units), str(sequence_axis)]
