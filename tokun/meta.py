a5020958be04ede6d6fd9f9aa8557d73f94c2d31
apehex <apehex@protonmail.com>
2024-08-01 17:41:43 +0200

    Add the global weight decay to the meta parameters

--- tokun/meta.py
+++ tokun/meta.py
@@ -3,6 +3,7 @@
 def rates(pretrained: bool=False, normalization: bool=True, base: float=0.001) -> tuple:
     return (
         (0.1 if pretrained else 1.) * (0.1 if normalization else 1.) * base, # lr max
+        0.1, # lambda => global decay
         0.9, # beta_1 => decay for the first moment
         0.99) # beta_2 => decay for the second moment
 
