ce6f34adf2f7e719266a33b2410302970ecdafb5
apehex <apehex@protonmail.com>
2024-08-12 20:07:28 +0200

    Verify that the encode & decode operations are reciprocal + test specific values
    Former-commit-id: 398a26d967dcd7c643f8e76290edca3137f9c67f

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,8 +1,58 @@
+import functools
+
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
 import tokun.pipeline
 
-# WITH CACHE ##################################################################
+# ENCODE ######################################################################
+
+class EncodeDecodeTest(tf.test.TestCase):
+    def setUp(self):
+        super(EncodeDecodeTest, self).setUp()
+        self._config = {'token_size': 16, 'sample_size': 1024}
+        # specify encoding parameters
+        self._encode = functools.partial(tokun.pipeline.encode, token_size=self._config['token_size'], sample_size=self._config['sample_size'], dtype=tf.int32)
+        # load the data
+        self._dataset_origin = tfds.load('mlqa/en', split='test', as_supervised=False, shuffle_files=True, data_dir='~/.cache/tensorflow/', batch_size=None)
+        # join the features
+        self._dataset_origin = self._dataset_origin.map(lambda __x: tf.strings.join([__x['context'], __x['question']], separator='\x1d'))
+        # encode the strings
+        self._dataset_encoded = self._dataset_origin.map(self._encode)
+        # decode back to the strings
+        self._dataset_decoded = self._dataset_encoded.map(tokun.pipeline.decode)
+
+    def test_dataset_specs(self):
+        # >
+        self.assertEqual(self._dataset_encoded.element_spec.shape, (4 * self._config['sample_size'],))
+        self.assertEqual(self._dataset_encoded.element_spec.dtype, tf.int32)
+        # <
+        self.assertEqual(self._dataset_decoded.element_spec.shape, ())
+        self.assertEqual(self._dataset_decoded.element_spec.dtype, tf.string)
+
+    def test_encode_decode_reciprocal(self):
+        __origin = iter(self._dataset_origin)
+        __decoded = iter(self._dataset_decoded)
+        for _ in range(16):
+            __o = next(__origin)
+            __d = next(__decoded)
+            # remove the padding
+            __o = __o.numpy().strip(b'\x00')
+            __d = __d.numpy().strip(b'\x00')
+            # check
+            self.assertEqual(__o[:self._config['sample_size']], __d[:self._config['sample_size']])
+
+    def test_specific_values(self):
+        __s = 'Hello world!'
+        __b = list(__s.encode('utf-32-be'))
+        __x = tf.convert_to_tensor(__s, dtype=tf.string)
+        __e = self._encode(__x)
+        __d = tokun.pipeline.decode(__e)
+        # ignore the padding
+        self.assertEqual(__b, __e.numpy().tolist()[:len(__b)])
+        self.assertEqual(__s, __d.numpy().decode('utf-8')[:len(__s)])
+
+# PREPROCESSING ###############################################################
 
 class PreprocessTest(tf.test.TestCase):
 
@@ -23,3 +73,5 @@ class PreprocessTest(tf.test.TestCase):
         __p1 = (-len(__s0) % 16) * 4
         self.assertAllClose(__x0[-__p0:], tf.zeros(shape=(__p0,), dtype=tf.dtypes.uint8))
         self.assertAllClose(__x1[-__p1:], tf.zeros(shape=(__p1,), dtype=tf.dtypes.uint8))
+
+# POSTPROCESSING ##############################################################
