3332be3118269c57e9717d61ecc59fd381b56f0e
apehex <apehex@protonmail.com>
2024-08-22 14:25:05 +0200

    Turn the byte values straight into embeddings

--- tokun/model.py
+++ tokun/model.py
@@ -5,6 +5,7 @@ import functools
 import keras
 import tensorflow as tf
 
+import mlable.layers.reshaping
 import tokun.layers
 
 # ENCODER #####################################################################
@@ -156,6 +157,116 @@ class AutoEncoder(tf.keras.models.Model):
 
     def get_config(self) -> dict:
         __config = super(AutoEncoder, self).get_config()
+        __config.update(self._config)
+        return __config
+
+    @classmethod
+    def from_config(cls, config) -> tf.keras.layers.Layer:
+        return cls(**config)
+
+# TOKUN ENCODER ###############################################################
+
+@keras.saving.register_keras_serializable(package='models')
+class TokunEncoder(tf.keras.models.Model):
+    def __init__(
+        self,
+        token_dim: int,
+        input_dim: int,
+        sequence_axis: int=1,
+        feature_axis: int=-1,
+        **kwargs
+    ) -> None:
+        # init
+        super(TokunEncoder, self).__init__(**kwargs)
+        # config
+        self._config = {
+            'token_dim': token_dim,
+            'input_dim': input_dim,
+            'sequence_axis': sequence_axis,
+            'feature_axis': feature_axis,}
+        # layers
+        self._factor = tf.cast(1. / input_dim, tf.float32)
+        self._divide = mlable.layers.reshaping.Divide(input_axis=sequence_axis, output_axis=feature_axis, factor=token_dim, insert=True, name='reshaping') # (B, S * T,) => (B, S, T)
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        return tf.cast(self._factor, inputs.dtype) * self._divide(inputs)
+
+    def get_config(self) -> dict:
+        __config = super(TokunEncoder, self).get_config()
+        __config.update(self._config)
+        return __config
+
+    @classmethod
+    def from_config(cls, config) -> tf.keras.layers.Layer:
+        return cls(**config)
+
+# TOKUN DECODER ###############################################################
+
+@keras.saving.register_keras_serializable(package='models')
+class TokunDecoder(tf.keras.models.Model):
+    def __init__(
+        self,
+        token_dim: int,
+        output_dim: int,
+        sequence_axis: int=1,
+        feature_axis: int=-1,
+        **kwargs
+    ) -> None:
+        # init
+        super(TokunDecoder, self).__init__(**kwargs)
+        # config
+        self._config = {
+            'token_dim': token_dim,
+            'output_dim': output_dim,
+            'sequence_axis': sequence_axis,
+            'feature_axis': feature_axis,}
+        # layers
+        self._factor = tf.cast(output_dim, tf.float32)
+        self._divide = mlable.layers.reshaping.Divide(input_axis=feature_axis, output_axis=sequence_axis, insert=False, factor=token_dim, name='reshaping') # (B, S, T * E) => (B, S * T, E)
+
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        return tf.cast(self._factor, inputs.dtype) * self._divide(inputs)
+
+    def get_config(self) -> dict:
+        __config = super(TokunDecoder, self).get_config()
+        __config.update(self._config)
+        return __config
+
+    @classmethod
+    def from_config(cls, config) -> tf.keras.layers.Layer:
+        return cls(**config)
+
+# VAE #########################################################################
+
+@keras.saving.register_keras_serializable(package='models')
+class Tokun(tf.keras.models.Model):
+    def __init__(
+        self,
+        token_dim: list,
+        input_dim: int,
+        output_dim: int,
+        sequence_axis: int=1,
+        feature_axis: int=-1,
+        **kwargs
+    ) -> None:
+        # init
+        super(Tokun, self).__init__(**kwargs)
+        # config
+        self._config = {
+            'token_dim': token_dim,
+            'input_dim': input_dim,
+            'output_dim': output_dim,
+            'sequence_axis': sequence_axis,
+            'feature_axis': feature_axis,}
+        # layers
+        self._encoder = TokunEncoder(token_dim=token_dim, input_dim=input_dim, sequence_axis=sequence_axis, feature_axis=feature_axis)
+        self._decoder = TokunDecoder(token_dim=token_dim, output_dim=output_dim, sequence_axis=sequence_axis, feature_axis=feature_axis)
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        return self._decoder(self._encoder(x))
+
+    def get_config(self) -> dict:
+        __config = super(Tokun, self).get_config()
         __config.update(self._config)
         return __config
 
