c1e4f43c578b30073d8ff0ab3ed3001eff396fb3
apehex <apehex@protonmail.com>
2025-04-27 18:13:07 +0200

    Exclude non allocated codepoints

--- tokun/data.py
+++ tokun/data.py
@@ -9,22 +9,22 @@ import tensorflow as tf
 
 # UNICODE #####################################################################
 
-def _random_codepoint_binary(lower_plane: int=0, upper_plane: int=0x40000) -> list:
+def _random_codepoint_binary(lower_plane: int=0, upper_plane: int=0x323af) -> list:
     __h = '{0:0>32b}'.format(int(random.uniform(lower_plane, upper_plane)))
     return [int(__b) for __b in __h]
 
-def _random_codepoint_bytes(lower_plane: int=0, upper_plane: int=0x40000) -> list:
+def _random_codepoint_bytes(lower_plane: int=0, upper_plane: int=0x323af) -> list:
     __h = '{0:0>8x}'.format(int(random.uniform(lower_plane, upper_plane)))
     return list(bytes.fromhex(__h))
 
-def random_codepoint(lower_plane: int=0, upper_plane: int=0x40000, binary: bool=False) -> list:
+def random_codepoint(lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> list:
     return _random_codepoint_binary(lower_plane=lower_plane, upper_plane=upper_plane) if binary else _random_codepoint_bytes(lower_plane=lower_plane, upper_plane=upper_plane)
 
-def random_sample(sample_size: int, lower_plane: int=0, upper_plane: int=0x40000, binary: bool=False) -> list:
+def random_sample(sample_size: int, lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> list:
     __nested = [random_codepoint(lower_plane=lower_plane, upper_plane=upper_plane, binary=binary) for _ in range(sample_size)]
     return list(itertools.chain.from_iterable(__nested))
 
-def random_dataset(sample_count: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x40000, binary: bool=False, dtype: tf.dtypes.DType=tf.int32) -> tf.data.Dataset:
+def random_dataset(sample_count: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False, dtype: tf.dtypes.DType=tf.int32) -> tf.data.Dataset:
     __factor = 32 if binary else 4
     # sample generator
     def __generator() -> iter:
