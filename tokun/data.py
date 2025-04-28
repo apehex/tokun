e7e364ad3179ee96623c4102fc07ec15d7414ea1
apehex <apehex@protonmail.com>
2025-04-28 14:09:03 +0200

    Return strings rather than byte encodings

--- tokun/data.py
+++ tokun/data.py
@@ -7,7 +7,7 @@ import random
 
 import tensorflow as tf
 
-# UNICODE #####################################################################
+# DECOMPOSITION ################################################################
 
 def _random_codepoint_binary(lower_plane: int=0, upper_plane: int=0x323af) -> list:
     __h = '{0:0>32b}'.format(int(random.uniform(lower_plane, upper_plane)))
@@ -17,20 +17,46 @@ def _random_codepoint_bytes(lower_plane: int=0, upper_plane: int=0x323af) -> lis
     __h = '{0:0>8x}'.format(int(random.uniform(lower_plane, upper_plane)))
     return list(bytes.fromhex(__h))
 
-def random_codepoint(lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> list:
-    return _random_codepoint_binary(lower_plane=lower_plane, upper_plane=upper_plane) if binary else _random_codepoint_bytes(lower_plane=lower_plane, upper_plane=upper_plane)
+# UNICODE ######################################################################
 
-def random_sample(sample_size: int, lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> list:
+def random_codepoint(lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> list:
+    return (
+        _random_codepoint_binary(lower_plane=lower_plane, upper_plane=upper_plane) if binary
+        else _random_codepoint_bytes(lower_plane=lower_plane, upper_plane=upper_plane))
+
+def random_character(lower_plane: int=0, upper_plane: int=0x323af) -> str:
+    __h = '{0:0>8x}'.format(int(random.uniform(lower_plane, upper_plane)))
+    return bytes.fromhex(__h).decode('utf-32-be', errors='replace')
+
+# SAMPLE #######################################################################
+
+def random_encoding(sample_size: int, lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> list:
     __nested = [random_codepoint(lower_plane=lower_plane, upper_plane=upper_plane, binary=binary) for _ in range(sample_size)]
     return list(itertools.chain.from_iterable(__nested))
 
-def random_dataset(sample_count: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False, dtype: tf.dtypes.DType=tf.int32) -> tf.data.Dataset:
+def random_string(sample_size: int, lower_plane: int=0, upper_plane: int=0x323af) -> list:
+    __nested = [random_character(lower_plane=lower_plane, upper_plane=upper_plane) for _ in range(sample_size)]
+    return ''.join(__nested)
+
+# DATASET ######################################################################
+
+def _random_dataset(sample_count: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x323af, binary: bool=False) -> tf.data.Dataset:
     __factor = 32 if binary else 4
     # sample generator
     def __generator() -> iter:
         for _ in range(sample_count):
-            yield random_sample(sample_size=sample_size, lower_plane=lower_plane, upper_plane=upper_plane, binary=binary)
+            yield random_encoding(sample_size=sample_size, lower_plane=lower_plane, upper_plane=upper_plane, binary=binary)
     # wrap into a dataset
     return tf.data.Dataset.from_generator(
         generator=__generator,
-        output_signature=tf.TensorSpec(shape=(__factor * sample_size,), dtype=dtype))
+        output_signature=tf.TensorSpec(shape=(__factor * sample_size,), dtype=tf.int32))
+
+def random_dataset(sample_count: int, sample_size: int, lower_plane: int=0, upper_plane: int=0x323af) -> tf.data.Dataset:
+    # sample generator
+    def __generator() -> iter:
+        for _ in range(sample_count):
+            yield random_string(sample_size=sample_size, lower_plane=lower_plane, upper_plane=upper_plane)
+    # wrap into a dataset
+    return tf.data.Dataset.from_generator(
+        generator=__generator,
+        output_signature=tf.TensorSpec(shape=(), dtype=tf.string))
